9c8552da9869262b7524b7473d9d0157285e9cc1
SimoneSpecchia <simone.specchia26@gmail.com>
2019-04-25 11:40:52 +0200

    Aggiunto nuove define e modificato s-functions uart

--- C_includes/pin_defines.h
+++ C_includes/pin_defines.h
@@ -2,6 +2,8 @@
 #define PINDEF_INCLUDE
 
 #include "main.h"
+//se il nome del pin e della define coincidono, commentare la riga della define
+//(segnandosi però che il nome è uguale)
 
 //Definizione pin per GearMotor
 #define GPIO_GEAR_MOTOR LED_4_GIALLO_GPIO_Port
@@ -16,12 +18,28 @@
 #define EFI_UPCUT_Pin   LED_1_GIALLO_Pin
 #define EFI_RPM_LIMITER_Pin LED_5_ROSSO_Pin
 
-//Definizione timer per clutch
+//Definizione pwm per clutch
 #define TIM_CLUTCH_CHANNEL TIM_CHANNEL_4
 #define hClutchTim htim4
 
 //Definizione uart utilizzata per debug
 #define huartDebug huart3
+
+//Definizione timer utilizzato per logica
+#define htim_1khz htim2
+#define TIM_1KHZ TIM2
+
+//Definizione ADC per sensori
+#define hadc_sensors hadc1
+#define ADC_SENSORS ADC1
+
+//Definizione led ausiliari su scheda
+//#define RedLed_GPIO_Port RedLed_GPIO_Port 
+//#define BlueLed_GPIO_Port BlueLed_GPIO_Port
+//#define GreenLed_GPIO_Port GreenLed_GPIO_Port
+//#define RedLed_Pin RedLed_Pin
+//#define BlueLed_Pin BlueLed_Pin
+//#define GreenLed_Pin GreenLed_Pin
 
 
 #endif
