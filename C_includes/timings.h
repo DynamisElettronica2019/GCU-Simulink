d65bec544068048bf1483d27d1af7bef20694640
SimoneSpecchia <simone.specchia26@gmail.com>
2019-04-26 13:04:19 +0200

    Aggiunto header file per tempi cambiata e parametri di acc

---
+++ C_includes/timings.h
@@ -0,0 +1,28 @@
+#define DEFAULT_DELAY       20
+#define DEFAULT_UP_REBOUND  15
+#define DEFAULT_UP_BRAKE    20
+#define DEFAULT_UP_PUSH_1_2 115
+#define DEFAULT_UP_PUSH_2_3 100
+#define DEFAULT_UP_PUSH_3_4 100
+#define DEFAULT_UP_PUSH_4_5 100
+     //down
+#define DEFAULT_CLUTCH      70
+#define DEFAULT_DN_PUSH     100
+#define DEFAULT_DN_BRAKE    15
+#define DEFAULT_DN_REBOUND  20
+     //neutral
+#define DEFAULT_NT_CLUTCH_DELAY 20
+#define DEFAULT_NT_REBOUND_1_N  15
+#define DEFAULT_NT_REBOUND_2_N  15
+#define DEFAULT_NT_BRAKE_1_N    35
+#define DEFAULT_NT_BRAKE_2_N    35
+#define DEFAULT_NT_PUSH_1_N     22
+#define DEFAULT_NT_PUSH_2_N     25
+#define DEFAULT_NT_CLUTCH_1_N   300
+#define DEFAULT_NT_CLUTCH_2_N   300
+     //Multiple tries
+#define DEFAULT_DOWN_TIME_CHECK 500
+#define DEFAULT_UP_TIME_CHECK   500
+#define DEFAULT_MAX_TRIES       2
+
+   
