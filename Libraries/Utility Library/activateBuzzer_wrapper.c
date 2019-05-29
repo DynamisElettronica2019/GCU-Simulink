44ad9b8ef03f84e30079270c672d3bbe16111b3f
SimoneSpecchia <simone.specchia26@gmail.com>
2019-05-29 13:30:29 +0200

    Aggiunti file s-function di buzzer a git

---
+++ Libraries/Utility Library/activateBuzzer_wrapper.c
@@ -0,0 +1,49 @@
+
+/*
+ * Include Files
+ *
+ */
+#if defined(MATLAB_MEX_FILE)
+#include "tmwtypes.h"
+#include "simstruc_types.h"
+#else
+#include "rtwtypes.h"
+#endif
+
+
+
+/* %%%-SFUNWIZ_wrapper_includes_Changes_BEGIN --- EDIT HERE TO _END */
+#if !defined(MATLAB_MEX_FILE)
+#include "pin_defines.h"
+#endif
+/* %%%-SFUNWIZ_wrapper_includes_Changes_END --- EDIT HERE TO _BEGIN */
+
+/*
+ * Create external references here.  
+ *
+ */
+/* %%%-SFUNWIZ_wrapper_externs_Changes_BEGIN --- EDIT HERE TO _END */
+#if !defined(MATLAB_MEX_FILE)
+extern TIM_HandleTypeDef hBuzzerTim;
+#endif
+/* %%%-SFUNWIZ_wrapper_externs_Changes_END --- EDIT HERE TO _BEGIN */
+
+/*
+ * Output function
+ *
+ */
+void activateBuzzer_Outputs_wrapper(void)
+{
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_BEGIN --- EDIT HERE TO _END */
+#if !defined(MATLAB_MEX_FILE)  
+  int pwmValue;
+  
+  pwmValue = hClutchTim.Init.Period/2;
+  
+  hBuzzerTim.Instance->CCR_BUZZER = pwmValue;
+  
+#endif
+/* %%%-SFUNWIZ_wrapper_Outputs_Changes_END --- EDIT HERE TO _BEGIN */
+}
+
+
