c062befe89a4df1638f0c00a24794398f19af259
Salvatore <salvatore.torsello08@gmail.com>
2019-06-21 19:03:00 +0200

    aggiunta file can_data.m al progetto e allo startup

---
+++ Script_matlab/can_data.m
@@ -0,0 +1,11 @@
+classdef can_data < Simulink.IntEnumType
+ enumeration
+  RPM          (0)
+  VH_SPEED     (1)
+  TPS          (2)
+  SLIP         (3)
+  SLIP_TARGET  (4)
+  GEAR_ADbits  (5)
+  GEAR         (6)
+ end    
+end
