522f11418402cf5e0a68d0da4ed8bb17819e93a6
SimoneSpecchia <simone.specchia26@gmail.com>
2019-07-04 19:23:03 +0200

    Corretto filtri

--- Libraries/CAN Library/CAN_Filter_Setup_wrapper.c
+++ Libraries/CAN Library/CAN_Filter_Setup_wrapper.c
@@ -38,12 +38,12 @@ void CAN_Filter_Setup_Outputs_wrapper(void)
 /* %%%-SFUNWIZ_wrapper_Outputs_Changes_BEGIN --- EDIT HERE TO _END */
 #if !defined(MATLAB_MEX_FILE)
 
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 14;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x304 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FC;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FC << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
@@ -51,94 +51,94 @@ void CAN_Filter_Setup_Outputs_wrapper(void)
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 15;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x30C << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FC;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FC << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 15;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
    
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 16;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x200 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FC;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FC << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 16;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 17;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x200 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FC;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FC << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 17;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 18;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x400 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FE;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FE << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 18;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 19;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x204 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FF;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FF << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 19;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 20;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x207 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FF;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FF << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 20;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
-  CAN_Filter_Config.FilterBank = CAN_FILTER_NUMBER;
+  CAN_Filter_Config.FilterBank = 21;
   CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDMASK;
   CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_32BIT;
   CAN_Filter_Config.FilterIdHigh = (0x700 << 5);
   CAN_Filter_Config.FilterIdLow = 0x0000;
-  CAN_Filter_Config.FilterMaskIdHigh = 0x3FF;//(0x7F0  << 5);
+  CAN_Filter_Config.FilterMaskIdHigh = (0x3FF << 5);//(0x7F0  << 5);
   CAN_Filter_Config.FilterMaskIdLow = 0x0000;
   CAN_Filter_Config.FilterFIFOAssignment = CAN_RX_FIFO0;
   CAN_Filter_Config.FilterActivation = ENABLE;	
-  CAN_Filter_Config.SlaveStartFilterBank = 21;
+  CAN_Filter_Config.SlaveStartFilterBank = 14;
   
   HAL_CAN_ConfigFilter(&hcan_active, &CAN_Filter_Config);
   
